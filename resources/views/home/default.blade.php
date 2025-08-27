d9cf279bf75c00fb8956c6903f4eec008e53bd82
Chinmoy Datta Priom <dattapro001@gmail.com>
2025-08-27 18:16:57 +0600

    add Google Tag Manager in default blade attempt 4 (2025-08-27 18:16:57)

--- resources/views/home/default.blade.php
+++ resources/views/home/default.blade.php
@@ -23,29 +23,7 @@
 
 
     <!-- Meta Pixel Base Code -->
-    {{-- <script>
-        ! function(f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window, document, 'script', '');
 
-        // fbq('init', '1036396911776364');
-        fbq('init', ''); // <-- replace with your pixel id if different
-        fbq('track', 'PageView');
-    </script> --}}
     <!-- Google Tag Manager -->
     <script>
         (function(w, d, s, l, i) {
