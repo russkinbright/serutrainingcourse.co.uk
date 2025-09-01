227fda0c9de42e54d5a5c4b6daae8adeb23ef084
Chinmoy Datta Priom <dattapro001@gmail.com>
2025-09-01 11:25:26 +0600

    Initialize window.dataLayer before GTM load in default blade (2025-09-01 11:25:26)

--- resources/views/home/default.blade.php
+++ resources/views/home/default.blade.php
@@ -1,126 +1,47 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png">
-    <link rel="icon" type="image/png" sizes="64x64" href="/favicon.png">
-    <link rel="shortcut icon" href="/favicon.png" type="image/x-icon">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    {{-- <title>Seru Training Course</title> --}}
+  {{-- Initialize dataLayer BEFORE GTM --}}
+  <script>window.dataLayer = window.dataLayer || [];</script>
 
-    <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
-    <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
-    <script src="https://cdn.quilljs.com/1.3.6/quill.min.js"></script>
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/purify-js@0.7.0/dist/purify.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-
-
-    <!-- Google Tag Manager -->
-    <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-NXQZKWMH');
-    </script>
-    <!-- End Google Tag Manager -->
-
-
-    <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=1036396911776364&ev=PageView&noscript=1" /></noscript>
-
-
-    {{-- Font-awesome --}}
-    <script src="https://kit.fontawesome.com/69ba9af9da.js" crossorigin="anonymous"></script>
-
-    <!-- lottie -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.12.2/lottie.min.js"></script>
-
-    <!-- PDF.js v2.16.105 only -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js"></script>
-    <script>
-        pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js';
-    </script>
-
-    {{-- Line Chart --}}
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-    <!-- Charts links -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-    <style>
-        [x-cloak] {
-            display: none !important;
-        }
-
-        #messageContainer {
-            background-color: rgba(0, 0, 0, 0.5);
-            /* Semi-transparent background */
-        }
-
-        #message {
-            animation: fadeInOut 2.5s ease-in-out;
-            /* Animation for fade in and out */
-        }
-
-        @keyframes fadeInOut {
-            0% {
-                opacity: 0;
-            }
-
-            10% {
-                opacity: 1;
-            }
-
-            90% {
-                opacity: 1;
-            }
-
-            100% {
-                opacity: 0;
-            }
-        }
-
-        .move {
-            transform: translateX(140%);
-        }
-    </style>
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];
+      w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NXQZKWMH');
+  </script>
+  <!-- End Google Tag Manager -->
+  ...
 </head>
-
 <body id="top">
-    @yield('content')
-</body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NXQZKWMH" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+  </noscript>
+  <!-- End Google Tag Manager (noscript) -->
 
-{!! $tawkScript ?? '' !!}
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API = Tawk_API || {},
-        Tawk_LoadStart = new Date();
-    (function() {
-        var s1 = document.createElement("script"),
-            s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/689b181ce0fd9f192a112a9f/1j2etcc7s';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
+  @yield('content')
+
+  {{-- Place all page-level pushes here --}}
+  @stack('scripts')
+
+  {!! $tawkScript ?? '' !!}
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+      var s1=document.createElement("script"), s0=document.getElementsByTagName("script")[0];
+      s1.async=true; s1.src='https://embed.tawk.to/689b181ce0fd9f192a112a9f/1j2etcc7s';
+      s1.charset='UTF-8'; s1.setAttribute('crossorigin','*'); s0.parentNode.insertBefore(s1,s0);
     })();
-</script>
-<!--End of Tawk.to Script-->
+  </script>
+  <!--End of Tawk.to Script-->
+</body>
+</html>
