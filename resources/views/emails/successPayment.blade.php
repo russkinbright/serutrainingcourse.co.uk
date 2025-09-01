5eca2917031fbfc01fb3b2d0fe33a8b172dd9917
Chinmoy Datta Priom <dattapro001@gmail.com>
2025-09-01 17:36:52 +0600

    remove cnd of tailwind from purchase page (2025-09-01 17:36:52)

--- resources/views/emails/successPayment.blade.php
+++ resources/views/emails/successPayment.blade.php
@@ -1,5 +1,4 @@
 @extends('home.default')
-    <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
